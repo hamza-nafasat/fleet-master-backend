9b4a4d411bf972f76ac004fa359beb1cd71dc5c0
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-09-24 17:43:11 +0500

    update code for enum for status value

--- src/models/subscriptionModel/subscription.model.ts
+++ src/models/subscriptionModel/subscription.model.ts
@@ -9,11 +9,7 @@ const subscriptionSchema = new Schema<SubscriberTypes>(
     stripeSubscriptionId: { type: String, required: true },
     paymentMethod: { type: [String] },
     priceId: { type: String, required: true },
-    subscriptionStatus: {
-      type: String,
-      enum: ["pending", "expired", "trial", "active", "past_due", "canceled", "unpaid"],
-      required: true,
-    },
+    subscriptionStatus: { type: String, required: true },
     billingAddress: { type: Map, of: String, required: true },
     subscriptionStartDate: { type: Date, default: Date.now, required: true },
     subscriptionEndDate: { type: Date, required: true },
